93b1fe3e6427562a926c0ec4a4baee9bdf7db0e5
jamie chen <jamie950315@gmail.com>
2025-04-28 17:02:32 +0800

    python code cleanup

--- rpi.py
+++ rpi.py
@@ -26,27 +26,14 @@ def fromArduino():
             print(f"Arduino: {resp}")
             lastReceivedTime=time.time()
             
-            if resp=="Arduino Ready":
+            if(resp=="Arduino Ready"):
                 endFlag=True
                 break
 
-        if endFlag or (time.time()-lastReceivedTime>timeoutDuration):
+        if(endFlag or (time.time()-lastReceivedTime>timeoutDuration)):
             break
 
         time.sleep(0.01)
-
-
-def OGfromArduino():
-    receivedFlag=False
-    start_time=time.time()
-    timeout_duration=3
-    while True:
-        while ser.in_waiting:
-            resp=ser.readline().decode()
-            print(f"Arduino 回傳: {resp}")
-            receivedFlag=True
-        if (receivedFlag or (time.time()-start_time>timeout_duration)):
-            break
 
 
 def toArduino(reply):
@@ -56,10 +43,10 @@ def toArduino(reply):
     else:
         commands=[]
 
-    if len(commands)>4:
+    if(len(commands)>4):
         commands=commands[-4:]
 
-    payload=" ".join(commands) + "\n"
+    payload=" ".join(commands)+"\n"
     ser.write(payload.encode())
 
 
@@ -74,24 +61,24 @@ def main():
 
     while True:
         userInput=input("You: ").strip()
-        if userInput.lower() in ("exit", "quit"):
+        if(userInput.lower() in ("exit", "quit")):
             print("End session.")
-            ser.close()             #############################################################################
+            ser.close()
             break
 
         prompt=userInput
 
         try:
-            params = {
+            params={
                 "model": "gpt-4.1-mini",
                 "instructions": instructions,
                 "input": prompt
             }
-            if not firstRun:
+            if(not firstRun):
                 params["previous_response_id"] = response.id
             response=client.responses.create(**params)
         except Exception as e:
-            print(f"Errot: {e}")
+            print(f"Error: {e}")
             continue
 
         reply=response.output_text
@@ -106,51 +93,4 @@ def main():
 
 if __name__=="__main__":
     main()
-
-
-
-
-
-
-
-
-
-# import json
-# from openai import OpenAI
-
-# client=OpenAI()
-
-# def main():
-#     prompt=""
-#     print("Start session, enter 'exit' to quit.\n")
-
-#     while True:
-#         user_input=input("You: ").strip()
-#         if user_input.lower() in ("exit", "quit"):
-#             print("End session.")
-#             break
-
-#         prompt+=f"User: {user_input}\nAssistant:"
-
-#         try:
-#             response=client.responses.create(
-#                 model="gpt-4.1-mini",
-#                 instructions="You are a helpful assistant.",
-#                 input=prompt
-                
-#             )
-#         except Exception as e:
-#             print(f"Errot: {e}")
-#             continue
-
-#         reply=response.output_text
-
-#         print(f"\nBot: {reply}\n")
-#         prompt+=f"{reply}\n"
-
-#         with open("chat_history.txt", "w", encoding="utf-8") as f:
-#             f.write(prompt)
-
-# if __name__=="__main__":
-#     main()
 
